0d71ecad13d94bb2b25b8c4006ab8b0b707aac72
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-06-17 15:07:31 -0300

    Agregando extras faltantes

---
+++ 00700_Functores vs predicados/extra.pl
@@ -0,0 +1,4 @@
+personaje(joffrey,lannister(310)).
+personaje(tommen,lannister(50)).
+personaje(cersei,lannister(450)).
+personaje(arya,stark(18,lobo(nymeria))).
---
+++ 00800_Diferentes aridades/extra.pl
@@ -0,0 +1,2 @@
+personaje(jon,nightwatch(23,lobo(ghost))).
+personaje(sam,nightwatch(23)).
