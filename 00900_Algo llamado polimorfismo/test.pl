c063cac9049122beb726ebc88cc0ce8aa5a8a005
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2015-06-17 16:17:59 -0300

    Corrigiendo test

--- 00900_Algo llamado polimorfismo/test.pl
+++ 00900_Algo llamado polimorfismo/test.pl
@@ -5,7 +5,7 @@ test('cuantoSabePersonaje(lannister(100), mucho) es verdadero') :-
   cuantoSabePersonaje(lannister(100), mucho).
 
 test('cuantoSabePersonaje(nigthwatch(23), poco) es verdadero') :-
-  cuantoSabePersonaje(lannister(23), Cuanto),
+  cuantoSabePersonaje(nigthwatch(23), Cuanto),
   assertion(Cuanto == poco).
 
 test('cuantoSabePersonaje(nigthwatch(23, lobo(ghost)), nada) es verdadero') :-
