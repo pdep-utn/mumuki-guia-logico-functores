b12d7fe9b77a85ab5ee81ec9618b875217b41c97
Nahuel Palumbo <nahuel.palumbo@gmail.com>
2015-06-18 05:32:33 -0300

    La flashie en el commit anterior

--- 01000_Algo llamado polimorfismo 2/extra.pl
+++ 01000_Algo llamado polimorfismo 2/extra.pl
@@ -5,9 +5,7 @@ personaje(arya,stark(18,lobo(nymeria))).
 personaje(jon,nightwatch(23,lobo(ghost))).
 personaje(sam,nightwatch(23)).
 
-cuantoSabePersonaje(joffrey,mucho).
-cuantoSabePersonaje(tommen,mucho).
-cuantoSabePersonaje(cersei,mucho).
-cuantoSabePersonaje(arya,poco).
-cuantoSabePersonaje(jon,nada).
-cuantoSabePersonaje(sam,poco).
+cuantoSabePersonaje(lannister(_), mucho).
+cuantoSabePersonaje(stark(_, _), poco).
+cuantoSabePersonaje(nightwatch(_), poco).
+cuantoSabePersonaje(nightwatch(_, lobo(_)), nada).
