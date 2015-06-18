bd6c06305dba3de4340b2227912613e99b429889
Nahuel Palumbo <nahuel.palumbo@gmail.com>
2015-06-18 05:27:48 -0300

    Se agrega el predicado cuantoSabePersonaje
    En el extra del punto 10.

--- 01000_Algo llamado polimorfismo 2/extra.pl
+++ 01000_Algo llamado polimorfismo 2/extra.pl
@@ -4,3 +4,10 @@ personaje(cersei,lannister(450)).
 personaje(arya,stark(18,lobo(nymeria))).
 personaje(jon,nightwatch(23,lobo(ghost))).
 personaje(sam,nightwatch(23)).
+
+cuantoSabePersonaje(joffrey,mucho).
+cuantoSabePersonaje(tommen,mucho).
+cuantoSabePersonaje(cersei,mucho).
+cuantoSabePersonaje(arya,poco).
+cuantoSabePersonaje(jon,nada).
+cuantoSabePersonaje(sam,poco).
